32920d6321dd75d9bbc04e72da6f5eb9cd69119b
JalalTabit <abdeljaliltabit2@gmail.com>
2023-03-21 16:39:06 -0700

    tables

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,18 +17,21 @@ _putchar(result + '0');
 }
 else if (result < 10)
 {
-_putchar(' ');
+_putchar(',');
 _putchar(' ');
 _putchar(result + '0');
 }
 else if (result >= 10 && result < 100)
 {
+_putchar(',');
 _putchar(' ');
 _putchar((result / 10) + '0');
 _putchar((result % 10) + '0');
 }
 else
 {
+_putchar(',');
+_putchar(' ');
 _putchar((result / 100) + '0');
 _putchar(((result % 100) / 10) + '0');
 _putchar((result % 10) + '0');
