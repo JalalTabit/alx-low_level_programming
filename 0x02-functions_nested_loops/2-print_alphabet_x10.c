be14f7e03dce05d7adca8cefbfb8584d31c034e5
JalalTabit <abdeljaliltabit2@gmail.com>
2023-03-21 10:19:45 -0700

    10x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet - Writes the alphabet in lowercase
+ * print_alphabet_x10 - Writes the alphabet in lowercase
  * Return: Always 0 (success)
  */
 void print_alphabet_x10(void)
