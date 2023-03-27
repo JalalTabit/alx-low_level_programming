e7e1bddb20a29e6d2cb9d0fc6846a91abdf6a5bc
JalalTabit <abdeljaliltabit2@gmail.com>
2023-03-27 09:35:23 -0700

    string copy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,11 +10,11 @@ char *_strcpy(char *dest, char *src)
 {
 	int len = 0;
 
-while (8(src + len) != '\0')
+while (*(src + len) != '\0')
 {
 	*(dest + len) = *(src + len);
 	len++;
 }
-*(dest + len) = '\0'
+*(dest + len) = '\0';
 return (dest);
 }
